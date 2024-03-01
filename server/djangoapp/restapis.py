60bf76d6f7e9f6e0c3d3388b6eea4147fc47b06a
Binay Humagain <humagain.binay@gmail.com>
2024-02-29 22:00:25 -0500

    Changes for lint

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,21 +6,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', 
-    default="https://humagainbina-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
+    "backend_url",
+    default="https://humagainbina-3030.theiadockernext-0-" \
+    "labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+)
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="https://sentianalyzer.1dr4z039akea.us-south.codeengine.appdomain.cloud/")
+    "sentiment_analyzer_url",
+    default="https://sentianalyzer.1dr4z039akea.us-south."\
+    "codeengine.appdomain.cloud/",
+)
 
 
 # def get_request(endpoint, **kwargs):
 def get_request(endpoint, **kwargs):
     params = ""
-    if (kwargs):
+    if kwargs:
         for key, value in kwargs.items():
-            params=params+key+ "=" +value+ "&"
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
@@ -35,7 +39,7 @@ def get_request(endpoint, **kwargs):
 
 # def analyze_review_sentiments(text):
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -43,12 +47,14 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+
+
 # Add code for retrieving sentiments
 
 
 # def post_review(data_dict):
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
@@ -56,5 +62,6 @@ def post_review(data_dict):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
-# Add code for posting review
 
+
+# Add code for posting review
